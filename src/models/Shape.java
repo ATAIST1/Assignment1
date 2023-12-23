6d1371c5ab0ec56e1e8cef83de681b717ac2550c
Ернар <230709@astanait.edu.kz>
2023-12-23 14:56:53 +0600

    Code returns the perimeter, the longest side, and an average length of a side of a shape formed by ten points in source.txt file.

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,44 +1,66 @@
 package models;
 import java.util.ArrayList;
-
 public class Shape {
     private ArrayList<Point> pointsArrayList;
+
+    // Constructor to initialize the Shape with an empty ArrayList of points
     public Shape(){
         pointsArrayList = new ArrayList<Point>();
     }
+
+    // Method to add a Point to the ArrayList of points
     public void addPoint(Point point) {
         pointsArrayList.add(point);
     }
+
+    // Method to calculate the perimeter of the shape
     public double calculatePerimeter() {
-        double per = 0;
-        for(int i = 0; i < 9; i++) {
-            per += pointsArrayList.get(i).distanceTo(pointsArrayList.get(i+1));
+        double perimeter = 0;
+
+        // Iterating through the first 10 points to calculate the sum of distances between consecutive points
+        for (int i = 0; i < 9; i++) {
+            perimeter += pointsArrayList.get(i).distanceTo(pointsArrayList.get(i + 1));
         }
-        per += pointsArrayList.get(0).distanceTo(pointsArrayList.get(9));
-        return per;
+
+        // Adding the distance between the last and first points to complete the perimeter calculation
+        perimeter += pointsArrayList.get(0).distanceTo(pointsArrayList.get(9));
+
+        return perimeter;
     }
 
+    // Method to find the longest distance between consecutive points
     public double getLongest() {
-        double max = 0;
+        double maxLength = 0;
         double side;
-        for(int i = 0; i < 9; i++) {
-            side = pointsArrayList.get(i).distanceTo(pointsArrayList.get(i+1));
-            if(max < side) {max = side;}
+
+        // Iterating through the first 10 points to find the longest distance
+        for (int i = 0; i < 9; i++) {
+            side = pointsArrayList.get(i).distanceTo(pointsArrayList.get(i + 1));
+            if (maxLength < side) {
+                maxLength = side;
+            }
         }
+
+        // Checking the distance between the last and first points
         side = pointsArrayList.get(0).distanceTo(pointsArrayList.get(9));
-        if(max < side) {max = side;}
-        return max;
+        if (maxLength < side) {
+            maxLength = side;
+        }
+
+        return maxLength;
     }
+
+    // Method to calculate the average distance between consecutive points
     public double getAverage() {
-        double min = 999999999;
         double sum = 0;
-        for(int i = 0; i < 9; i++) {
+
+        // Iterating through the first 10 points to calculate the sum of distances
+        for (int i = 0; i < 9; i++) {
             sum += pointsArrayList.get(i).distanceTo(pointsArrayList.get(i + 1));
         }
+        sum += pointsArrayList.get(0).distanceTo(pointsArrayList.get(9));
+
+        // Returning the average distance
         return sum / 10;
     }
-
-
-
-
 }
