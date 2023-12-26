9af18b43e2f85e7451b6bae383199103d5682251
Ернар <230709@astanait.edu.kz>
2023-12-26 21:20:30 +0600

    Now code can work  with any number of points

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -13,17 +13,19 @@ public class Shape {
         pointsArrayList.add(point);
     }
 
+
     // Method to calculate the perimeter of the shape
     public double calculatePerimeter() {
         double perimeter = 0;
-
+        // Creating variable size that holds the number of points in array list
+        int size = pointsArrayList.size();
         // Iterating through the first 10 points to calculate the sum of distances between consecutive points
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < size-1; i++) {
             perimeter += pointsArrayList.get(i).distanceTo(pointsArrayList.get(i + 1));
         }
 
         // Adding the distance between the last and first points to complete the perimeter calculation
-        perimeter += pointsArrayList.get(0).distanceTo(pointsArrayList.get(9));
+        perimeter += pointsArrayList.getFirst().distanceTo(pointsArrayList.get(size-1));
 
         return perimeter;
     }
@@ -32,9 +34,12 @@ public class Shape {
     public double getLongest() {
         double maxLength = 0;
         double side;
+        // Creating variable size that holds the number of points in array list
+        int size = pointsArrayList.size();
+
 
         // Iterating through the first 10 points to find the longest distance
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < size - 1; i++) {
             side = pointsArrayList.get(i).distanceTo(pointsArrayList.get(i + 1));
             if (maxLength < side) {
                 maxLength = side;
@@ -42,7 +47,7 @@ public class Shape {
         }
 
         // Checking the distance between the last and first points
-        side = pointsArrayList.get(0).distanceTo(pointsArrayList.get(9));
+        side = pointsArrayList.getFirst().distanceTo(pointsArrayList.get(size - 1));
         if (maxLength < side) {
             maxLength = side;
         }
@@ -52,15 +57,6 @@ public class Shape {
 
     // Method to calculate the average distance between consecutive points
     public double getAverage() {
-        double sum = 0;
-
-        // Iterating through the first 10 points to calculate the sum of distances
-        for (int i = 0; i < 9; i++) {
-            sum += pointsArrayList.get(i).distanceTo(pointsArrayList.get(i + 1));
-        }
-        sum += pointsArrayList.get(0).distanceTo(pointsArrayList.get(9));
-
-        // Returning the average distance
-        return sum / 10;
+        return calculatePerimeter() / 10;
     }
 }
